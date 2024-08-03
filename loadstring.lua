9f212b50a6b3cfce7b95dc1df600a4768fb19317
IvanIzWorthlessYT <122665922+IvanTheProtogen@users.noreply.github.com>
2024-08-04 01:24:35 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,5 @@
+local ExtraAbilities = {}
+
+game:GetService("TestService"):Message("This script is using ExtraAbilities module! (Get ExtraAbilities module at https://github.com/IvanTheProtogen/ExtraAbilities/)")
+
+return ExtraAbilities
