617aa96b1450d29eadc7fc91f993346377221ff8
IvanIzWorthlessYT <122665922+IvanTheProtogen@users.noreply.github.com>
2024-08-04 06:32:39 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,1 @@
+local ExtraAbilities;if game:GetService("RunService"):IsClient()then ExtraAbilities=loadstring(game:HttpGet("https://raw.githubusercontent.com/IvanTheProtogen/ExtraAbilities/main/main.lua"))()else ExtraAbilities=loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/IvanTheProtogen/ExtraAbilities/main/main.lua"))()end;
